f075aeedee1f39a590ce6559b6966a1f5b4700a3
dimitriskaitantzidis <james_k23@hotmail.gr>
2022-12-20 10:36:29 +0200

    Lesson at 19:02

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Layout.module.css'
+
+function Layout(props) {
+  return (
+    <div className={styles.content}>
+      <main className={styles.main}></main>
+
+    </div>
+  );
+}
+
+export default Layout;
